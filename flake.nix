8ed0dc968cf7306faa08815fcb298f4c8681c79f
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-05-24 21:56:23 +0530

    Changed from master branch to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,62 +2,63 @@
   description = "A flake providing an up-to-date package for zed-editor";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     patched-nixpkgs.url = "github:TomaSajt/nixpkgs?ref=fetch-cargo-vendor-dup";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    nixpkgs,
-    patched-nixpkgs,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} (
-      {
-        withSystem,
-        moduleWithSystem,
-        flake-parts-lib,
-        ...
+  outputs =
+    inputs @ { flake-parts
+    , nixpkgs
+    , patched-nixpkgs
+    , ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem
+      , moduleWithSystem
+      , flake-parts-lib
+      , ...
       }: {
         systems = with nixpkgs.lib.platforms; linux ++ darwin;
 
-        perSystem = {
-          system,
-          pkgs,
-          self',
-          inputs',
-          ...
-        }: let
-          rustPlatform = inputs'.patched-nixpkgs.legacyPackages.rustPlatform;
-        in {
-          packages = {
-            zed-editor = pkgs.callPackage ./packages/zed-editor {rustPlatform = rustPlatform;};
-            zed-editor-fhs = self'.packages.zed-editor.passthru.fhs;
+        perSystem =
+          { system
+          , pkgs
+          , self'
+          , inputs'
+          , ...
+          }:
+          let
+            rustPlatform = inputs'.patched-nixpkgs.legacyPackages.rustPlatform;
+          in
+          {
+            packages = {
+              zed-editor = pkgs.callPackage ./packages/zed-editor { rustPlatform = rustPlatform; };
+              zed-editor-fhs = self'.packages.zed-editor.passthru.fhs;
 
-            zed-editor-preview = pkgs.callPackage ./packages/zed-editor-preview {rustPlatform = rustPlatform;};
-            zed-editor-preview-fhs = self'.packages.zed-editor-preview.passthru.fhs;
+              zed-editor-preview = pkgs.callPackage ./packages/zed-editor-preview { rustPlatform = rustPlatform; };
+              zed-editor-preview-fhs = self'.packages.zed-editor-preview.passthru.fhs;
 
-            zed-editor-bin = pkgs.callPackage ./packages/zed-editor-bin {};
-            zed-editor-bin-fhs = self'.packages.zed-editor-bin.passthru.fhs;
+              zed-editor-bin = pkgs.callPackage ./packages/zed-editor-bin { };
+              zed-editor-bin-fhs = self'.packages.zed-editor-bin.passthru.fhs;
 
-            zed-editor-preview-bin = pkgs.callPackage ./packages/zed-editor-preview-bin {};
-            zed-editor-preview-bin-fhs = self'.packages.zed-editor-preview-bin.passthru.fhs;
+              zed-editor-preview-bin = pkgs.callPackage ./packages/zed-editor-preview-bin { };
+              zed-editor-preview-bin-fhs = self'.packages.zed-editor-preview-bin.passthru.fhs;
 
-            default = self'.packages.zed-editor;
+              default = self'.packages.zed-editor;
+            };
+
+            apps = {
+              zed-editor.program = "${self'.packages.zed-editor}/bin/zeditor";
+              zed-editor-fhs.program = "${self'.packages.zed-editor-fhs}/bin/zeditor";
+              zed-editor-preview.program = "${self'.packages.zed-editor-preview}/bin/zeditor";
+              zed-editor-preview-fhs.program = "${self'.packages.zed-editor-preview-fhs}/bin/zeditor";
+              zed-editor-bin.program = "${self'.packages.zed-editor-bin}/bin/zeditor";
+              zed-editor-bin-fhs.program = "${self'.packages.zed-editor-bin-fhs}/bin/zeditor";
+              zed-editor-preview-bin.program = "${self'.packages.zed-editor-preview-bin}/bin/zeditor";
+              zed-editor-preview-bin-fhs.program = "${self'.packages.zed-editor-preview-bin-fhs}/bin/zeditor";
+            };
           };
-
-          apps = {
-            zed-editor.program = "${self'.packages.zed-editor}/bin/zeditor";
-            zed-editor-fhs.program = "${self'.packages.zed-editor-fhs}/bin/zeditor";
-            zed-editor-preview.program = "${self'.packages.zed-editor-preview}/bin/zeditor";
-            zed-editor-preview-fhs.program = "${self'.packages.zed-editor-preview-fhs}/bin/zeditor";
-            zed-editor-bin.program = "${self'.packages.zed-editor-bin}/bin/zeditor";
-            zed-editor-bin-fhs.program = "${self'.packages.zed-editor-bin-fhs}/bin/zeditor";
-            zed-editor-preview-bin.program = "${self'.packages.zed-editor-preview-bin}/bin/zeditor";
-            zed-editor-preview-bin-fhs.program = "${self'.packages.zed-editor-preview-bin-fhs}/bin/zeditor";
-          };
-        };
       }
     );
 }
